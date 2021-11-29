8929d88a6a3c603eb5c41a328be4d4517ac7b4bd
Jeffrey Larson <jmlarson@anl.gov>
2021-11-29 14:39:31 -0600

    Adding some clarifying text

--- libensemble/tests/regression_tests/test_persistent_ytopt_gen.py
+++ libensemble/tests/regression_tests/test_persistent_ytopt_gen.py
@@ -1,17 +1,17 @@
 """
-Runs libEnsemble with Latin hypercube sampling on a simple 1D problem
+Runs libEnsemble to call the ytopt ask/tell interface in a generator function,
+and the ytopt findRunTime interface in a simulator function.
 
 Execute via one of the following commands (e.g. 3 workers):
-   mpiexec -np 4 python3 test_1d_sampling.py
-   python3 test_1d_sampling.py --nworkers 3 --comms local
-   python3 test_1d_sampling.py --nworkers 3 --comms tcp
+   mpiexec -np 4 python3 test_persistent_ytopt_gen.py
+   python3 test_persistent_ytopt_gen.py --nworkers 3 --comms local
 
 The number of concurrent evaluations of the objective function will be 4-1=3.
 """
 
 # Do not change these lines - they are parsed by run-tests.sh
-# TESTSUITE_COMMS: mpi local tcp
-# TESTSUITE_NPROCS: 2 4
+# TESTSUITE_COMMS: mpi
+# TESTSUITE_NPROCS: 4
 
 import numpy as np
 
@@ -29,22 +29,23 @@ from ytopt.search.optimizer import Optimizer
 nworkers, is_manager, libE_specs, _ = parse_args()
 num_sim_workers = nworkers - 1  # Subtracting one because one worker will be the generator
 
+# Declare the sim_f to be optimized, and the input/outputs
 sim_specs = {
     'sim_f': sim_f,
     'in': ['BLOCK_SIZE'],
     'out': [('RUN_TIME', float)],
 }
 
-
+# Initialize the ytopt ask/tell interface (to be used by the gen_f)
 cs = CS.ConfigurationSpace(seed=1234)
 p0 = CSH.UniformIntegerHyperparameter(name='BLOCK_SIZE', lower=1, upper=10, default_value=5)
 cs.add_hyperparameters([p0])
 input_space = cs
-
 ytoptimizer = Optimizer(
     num_workers=num_sim_workers, space=input_space, learner='RF', liar_strategy='cl_max', acq_func='gp_hedge'
 )
 
+# Declare the gen_f that will generator points for the sim_f, and the various input/outputs
 gen_specs = {
     'gen_f': gen_f,
     'out': [('BLOCK_SIZE', int, (1,))],
@@ -61,7 +62,7 @@ persis_info = add_unique_random_streams({}, nworkers + 1, seed=1234)
 
 exit_criteria = {'sim_max': 10}
 
-# Perform the run
+# Perform the libE run
 H, persis_info, flag = libE(sim_specs, gen_specs, exit_criteria, persis_info, alloc_specs, libE_specs=libE_specs)
 
 if is_manager:
